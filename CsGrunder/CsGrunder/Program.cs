fe6cf3d9c3e0ca9635fa10cf31b46d1165e90a27
Erik Stenbäcka <erik.stenbacka@sigma.se>
2021-09-13 13:23:44 +0200

    Add solution proposals for week 1 day 1 exercises

---
+++ CsGrunder/CsGrunder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+// Exercise: Installera Visual Studio
+Console.WriteLine("What is your name?");
+
+
+
+// Exercise: Input - part 1
+Console.WriteLine("What is your name?");
+var userName = Console.ReadLine();
+
+Console.WriteLine("Hej " + userName);
+
+
+
+// Exercise: Input - part 2
+var var1 = Console.ReadLine();
+var var2 = Console.ReadLine();
+var var3 = Console.ReadLine();
+var output = var3 + " " + var2 + " " + var1;
+Console.WriteLine(output);
