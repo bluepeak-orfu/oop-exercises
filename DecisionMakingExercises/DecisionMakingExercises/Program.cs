f046cdc48fdadc608cf58f3153ff2d6ce0a1a8b6
Erik Stenbäcka <erik.stenbacka@sigma.se>
2021-09-16 11:52:10 +0200

    Add solution proposals for decision making exercises

---
+++ DecisionMakingExercises/DecisionMakingExercises/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+//// Exercise: IF:ar - 1
+//var input = Console.ReadLine();
+
+//var inputAsInt = int.Parse(input);
+
+//if (inputAsInt < 18)
+//{
+//    Console.WriteLine("Åtkomst nekad");
+//}
+
+
+
+//// Exercise: IF:ar - 2
+//var input = Console.ReadLine();
+
+//var inputAsInt = int.Parse(input);
+
+//if (inputAsInt % 2 == 0) {
+//    Console.WriteLine("J");
+//} else
+//{
+//    Console.WriteLine("O");
+//}
+
+
+
+//// Exercise: IF:ar - 3
+//var input = Console.ReadLine();
+//if (input == "")
+//{
+//    Console.WriteLine("Felaktig input");
+//}
+//else
+//{
+//    var inputAsInt = int.Parse(input);
+
+//    if (inputAsInt >= 1 && inputAsInt <= 5)
+//    {
+//        Console.WriteLine("Top 5!");
+//    }
+//    else if (inputAsInt > 5 && inputAsInt <= 10)
+//    {
+//        Console.WriteLine("Top 10!");
+//    }
+//}
+
+
+
+//// Exercise: Miniräknare
+//Console.WriteLine("Skriv en operator (+, -)");
+//var operat = Console.ReadLine();
+
+//Console.WriteLine("Skriv ett nummer:");
+//var number1 = int.Parse(Console.ReadLine());
+
+//Console.WriteLine("Skriv ett till nummer:");
+//var number2 = int.Parse(Console.ReadLine());
+
+//switch (operat)
+//{
+//    case "+":
+//        Console.WriteLine(number1 + number2);
+//        break;
+//    case "-":
+//        Console.WriteLine(number1 - number2);
+//        break;
+//}
