569dce9aa26b74b491de684377602a3cfcebec52
Erik Stenbäcka <erik.stenbacka@sigma.se>
2021-09-27 10:56:19 +0200

    Add solution proposal for Klasser - grunder

---
+++ OOGrunderExercises/OOGrunderExercises/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOGrunderExercises
+{
+    class Person
+    {
+        private string _name;
+        private int _age;
+
+        public Person(string name, int age)
+        {
+            _name = name;
+            _age = age;
+        }
+    }
+}
---
+++ OOGrunderExercises/OOGrunderExercises/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using OOGrunderExercises;
+
+Person person1 = new Person("Olle", 12);
+Person person2 = new Person("Olga", 33);
+
+Account account = new Account();
+account._amount = 1234.56;
