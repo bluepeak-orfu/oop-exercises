bfe13b74bb7c2e08134f8042e366962e20811c92
Erik Stenbäcka <erik.stenbacka@sigma.se>
2021-09-27 08:43:44 +0200

    Pseudo code example

--- SwordsNBarbarians/SwordsNBarbarians/Program.cs
+++ SwordsNBarbarians/SwordsNBarbarians/Program.cs
@@ -1,2 +1,22 @@
 ﻿using System;
 
+// Läs in användaren namn till playerName
+
+// loopa medan båda spelarna är vid liv
+  // Skriv ut spelplanen
+
+  // Om det är spelarens tur
+    // Läs in användarens val till action
+  // Annars
+    // Vänta 500 ms innan nästa steg
+    // Slumpa fram motståndarens action
+
+  // Beräkna skada på motstådaren
+  // Om action är 1 beräkna skada till 2
+  // Annars om action är 2 beräkna skada till 2 * (0 till 3)
+
+  // Gå ur loopen om någon av spelarna har slut på liv
+
+  // Växla nuvarande spelare
+
+// Skriv ut vinnarens namn
