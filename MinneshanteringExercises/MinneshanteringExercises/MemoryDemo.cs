54fb090ea7e186d2f8d773e4a41e0947060214dd
Erik Stenbäcka <erik.stenbacka@sigma.se>
2021-10-04 09:33:38 +0200

    Add solution proposal

---
+++ MinneshanteringExercises/MinneshanteringExercises/MemoryDemo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinneshanteringExercises
+{
+    class MemoryDemo
+    {
+        public int MyValue { get; init; }
+    }
+}
---
+++ MinneshanteringExercises/MinneshanteringExercises/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using MinneshanteringExercises;
+
+int x = 2;
+double y = 0.34;
+
+string q = "abc";
+int[] w = new int[3];
+
+
+int i1 = 8;
+int i2 = 8;
+
+if (i1 == i2)
+{
+    Console.WriteLine("Value type är lika");
+}
+
+MemoryDemo m1 = new MemoryDemo() { MyValue = 8 };
+MemoryDemo m2 = new MemoryDemo() { MyValue = 8 };
+
+if (m1 == m2)
+{
+    Console.WriteLine("Reference type är lika");
+}
+
+if (m1.MyValue == m2.MyValue)
+{
+    Console.WriteLine("Value type i Reference type är lika");
+}
