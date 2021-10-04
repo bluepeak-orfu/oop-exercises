ddd1f8d1036bed4120c1975aca7cee5707f3b7ef
Erik Stenbäcka <erik.stenbacka@sigma.se>
2021-10-04 10:10:28 +0200

    Add solution proposal to null exercises

--- MinneshanteringExercises/MinneshanteringExercises/Program.cs
+++ MinneshanteringExercises/MinneshanteringExercises/Program.cs
@@ -1,30 +1,60 @@
 ﻿using System;
 using MinneshanteringExercises;
 
-int x = 2;
-double y = 0.34;
+//int x = 2;
+//double y = 0.34;
 
-string q = "abc";
-int[] w = new int[3];
+//string q = "abc";
+//int[] w = new int[3];
 
 
-int i1 = 8;
-int i2 = 8;
+//int i1 = 8;
+//int i2 = 8;
 
-if (i1 == i2)
+//if (i1 == i2)
+//{
+//    Console.WriteLine("Value type är lika");
+//}
+
+//MemoryDemo m1 = new MemoryDemo() { MyValue = 8 };
+//MemoryDemo m2 = new MemoryDemo() { MyValue = 8 };
+
+//if (m1 == m2)
+//{
+//    Console.WriteLine("Reference type är lika");
+//}
+
+//if (m1.MyValue == m2.MyValue)
+//{
+//    Console.WriteLine("Value type i Reference type är lika");
+//}
+
+
+// Exercise: Null - 1
+string s = null;
+if (s == null)
 {
-    Console.WriteLine("Value type är lika");
+    Console.WriteLine("string is null");
 }
 
-MemoryDemo m1 = new MemoryDemo() { MyValue = 8 };
-MemoryDemo m2 = new MemoryDemo() { MyValue = 8 };
-
-if (m1 == m2)
+// Exercise: Null - 2
+string[] stringArray = new string[3];
+stringArray[0] = "abc";
+stringArray[2] = "def";
+foreach (string item in stringArray)
 {
-    Console.WriteLine("Reference type är lika");
+    if (item != null)
+    {
+        Console.WriteLine(item);
+    }
 }
 
-if (m1.MyValue == m2.MyValue)
+// Exercise: Null - 3
+MemoryDemo[] demoArray = new MemoryDemo[3];
+demoArray[0] = new MemoryDemo() { MyValue = 12 };
+demoArray[2] = new MemoryDemo() { MyValue = 23 };
+
+foreach (MemoryDemo item in demoArray)
 {
-    Console.WriteLine("Value type i Reference type är lika");
+    Console.WriteLine(item?.MyValue);
 }
