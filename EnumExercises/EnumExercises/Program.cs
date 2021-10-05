f58cd61e6600e15ba7698d4cb1aa63b4cc3186e0
Erik Stenbäcka <erik.stenbacka@sigma.se>
2021-10-05 12:54:18 +0200

    Add solution proposals for enum exercises

---
+++ EnumExercises/EnumExercises/Program.cs
@@ -0,0 +1,29 @@
+﻿using EnumExercises;
+using System;
+
+OrderStatus myPizzaOrder = OrderStatus.Ordered;
+
+while (myPizzaOrder != OrderStatus.Delivered)
+{
+    switch (myPizzaOrder)
+    {
+        case OrderStatus.Ordered:
+            Console.WriteLine("Is the pizza ready?");
+            string readyResponse = Console.ReadLine();
+            if (readyResponse == "y")
+            {
+                myPizzaOrder = OrderStatus.Ready;
+            }
+            break;
+        case OrderStatus.Ready:
+            Console.WriteLine("Is the pizza delivered?");
+            string deliveredResponse = Console.ReadLine();
+            if (deliveredResponse == "y")
+            {
+                myPizzaOrder = OrderStatus.Delivered;
+            }
+            break;
+        case OrderStatus.Delivered:
+            break;
+    }
+}
