deb5d696e298d83e4e9954bf9de50b3ae8d79eef
Erik Stenbäcka <erik.stenbacka@sigma.se>
2021-10-05 12:48:03 +0200

    Add solution proposals for list exercises

---
+++ OOOtherExercises/OOOtherExercises/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+//// Exercises: listor - 1
+//List<int> myList = new List<int>() { 1, 2, 3, 4, 5 };
+////List<int> myList = new List<int>();
+////myList.Add(1);
+////myList.Add(2);
+////myList.Add(3);
+////myList.Add(4);
+////myList.Add(5);
+
+
+//// Exercises: listor - 2
+//Console.WriteLine($"Index 2: {myList[2]}");
+
+
+//// Exercises: listor - 3
+//myList.RemoveAt(0);
+
+
+//// Exercises: listor - 4
+//myList.AddRange(new int[] { 10, 20, 30 });
+
+
+//// Exercises: listor - 5
+//Console.WriteLine($"List length: {myList.Count}");
+
+
+//// Exercises: listor - 6
+//foreach (int item in myList)
+//{
+//    if (item % 2 == 0)
+//    {
+//        Console.WriteLine("hej");
+//    }
+//}
+
+
+
+
