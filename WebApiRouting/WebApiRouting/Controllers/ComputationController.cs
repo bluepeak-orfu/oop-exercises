6a09425258f0e86e1538f1ee87f7a572907d4313
Erik Stenbäcka <erik.stenbacka@nexergroup.com>
2021-10-26 21:00:10 +0200

    Add solution propsal for part 1 of web api routing exercise

---
+++ WebApiRouting/WebApiRouting/Controllers/ComputationController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiBasics.Controllers
+{
+    [Route("compute")]
+    [ApiController]
+    public class ComputationController : ControllerBase
+    {
+        [HttpGet("message")]
+        public string Message(string myMessage)
+        {
+            return $"Ditt meddelande är: {myMessage}";
+        }
+
+        [HttpGet("upper/{message}")] 
+        public string Upper(string message)
+        {
+            // /compute/upper/hej hej hej
+            // compute <- Mappar till klass
+            // upper
+            // hej hej hej
+            return message.ToUpper();
+        }
+
+        [HttpGet("concat/{message1}")]
+        public string Concat(string message1, string message2)
+        {
+            return message1 + message2;
+        }
+
+        [HttpGet("add")]
+        public string Add(int value1, int value2)
+        {
+            int sum = value1 + value2;
+            return sum.ToString();
+            // return $"{value1 + value2}";
+        }
+
+        [HttpGet("add2/{value1}/{value2}")]
+        public string Add2(int value1, int value2)
+        {
+            int sum = value1 + value2;
+            return sum.ToString();
+            // return $"{value1 + value2}";
+        }
+
+        [HttpGet("multi")]
+        public string MultiGreeter(int count, string message)
+        {
+            string result = "";
+            for (int i = 0; i < count; i++)
+            {
+                result += message;
+            }
+            return result;
+        }
+
+        [HttpGet("multi2/{count}/{message}")]
+        public string MultiGreeter2(int count, string message)
+        {
+            string result = "";
+            for (int i = 0; i < count; i++)
+            {
+                result += message;
+            }
+            return result;
+        }
+    }
+}
