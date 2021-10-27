091cc3f6d80bd09aa838f145fc84ed4d1ee4bf4d
Erik Stenbäcka <erik.stenbacka@sigma.se>
2021-10-27 12:20:00 +0200

    Part 1 of web api mapping

---
+++ WebApiMappings/WebApiMappings/Controllers/PersonController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiMappings.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet("get")]
+        public Person GetPerson()
+        {
+            Person person;
+            person = new Person()
+            {
+                Name = "Pelle",
+                Age = 12
+            };
+            return person;
+        }
+
+        [HttpGet("list")]
+        public List<Person> ListPersons()
+        {
+            return new List<Person>()
+            {
+                new Person() { Name = "Adam", Age = 22 },
+                new Person() { Name = "Berit", Age = 33 },
+                new Person() { Name = "Calle", Age = 44 }
+            };
+        }
+
+        [HttpPost("create")]
+        public bool CreatePerson([FromBody] Person person)
+        {
+            return true;
+        }
+    }
+}
---
+++ WebApiMappings/WebApiMappings/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiMappings
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
