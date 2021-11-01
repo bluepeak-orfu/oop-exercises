5c762c6d8becd090fe4436082e9b1dd7b2c5adcd
Erik Stenbäcka <erik.stenbacka@sigma.se>
2021-11-01 10:02:00 +0100

    Add solution proposal for part 4 of train travel

---
+++ TrainTravelCo/TrainTravelCo/Controllers/BookingController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TrainTravelCo.Managers;
+using TrainTravelCo.Models;
+
+namespace TrainTravelCo.Controllers
+{
+    [Route("booking")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        private BookingManager _bookingManager;
+
+        public BookingController()
+        {
+            _bookingManager = new BookingManager();
+        }
+
+        [HttpGet]
+        public List<Trip> Search(string from)
+        {
+            return _bookingManager.Search(from);
+        }
+    }
+}
---
+++ TrainTravelCo/TrainTravelCo/Managers/BookingManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TrainTravelCo.Data;
+using TrainTravelCo.Models;
+
+namespace TrainTravelCo.Managers
+{
+    public class BookingManager
+    {
+        public List<Trip> Search(string from)
+        {
+            List<Trip> result = new List<Trip>();
+            List<Trip> allTrips = DataStore.Instance.ListTrips();
+            foreach (Trip trip in allTrips)
+            {
+                if (trip.From == from)
+                {
+                    result.Add(trip);
+                }
+            }
+            return result;
+        }
+    }
+}
