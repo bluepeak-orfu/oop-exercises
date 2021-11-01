e85f32ef9bcd09c12db41c00c0e9be4285050204
Erik Stenbäcka <erik.stenbacka@nexergroup.com>
2021-11-01 06:49:48 +0100

    Add exercise console file explorer

---
+++ ConsoleFileExplorer/ConsoleFileExplorer/ConsoleExplorer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleFileExplorer
+{
+    class ConsoleExplorer
+    {
+        private FolderView _currentView;
+
+        public void Run()
+        {
+            _currentView = new FolderView(".");
+            while (true)
+            {
+                string[] dirs = Directory.GetFileSystemEntries(".");
+                foreach (string dirName in dirs)
+                {
+                    if (File.Exists(dirName))
+                    {
+                        Console.WriteLine($"- {Path.GetFileName(dirName)}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"# {Path.GetFileName(dirName)}");
+                    }
+                }
+                //string[] fileNames = Directory.GetFiles(".");
+                //foreach (string fileName in fileNames)
+                //{
+                //    Console.WriteLine($"- {Path.GetFileName(fileName)}");
+                //}
+                //List<string> currentContent = _currentView.GetFolderContent();
+                //foreach (string item in currentContent)
+                //{
+                //    Console.WriteLine(item);
+                //}
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ ConsoleFileExplorer/ConsoleFileExplorer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleFileExplorer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new ConsoleExplorer().Run();
+        }
+    }
+}
