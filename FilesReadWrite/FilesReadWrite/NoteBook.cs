356bb43557656223723a6cb9523a1062cde0a73e
Erik Stenbäcka <erik.stenbacka@sigma.se>
2021-11-02 08:57:43 +0100

    Add solution proposal for files read write

---
+++ FilesReadWrite/FilesReadWrite/NoteBook.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesReadWrite
+{
+    class NoteBook
+    {
+        private readonly string _fileName = "notes.txt";
+
+        public void TakeNote(string note)
+        {
+            string outputFormat = "{0,-24}{1}";
+
+            if (!File.Exists(_fileName))
+            {
+                string[] headerInfo = new string[]
+                {
+                    $"{"Datum",-24}{"Note"}", // <-- string interpolation
+                    //string.Format(outputFormat, "Datum", "Note"), // <-- string composition
+                    new string('-', 60)
+                };
+                File.WriteAllLines(_fileName, headerInfo);
+            }
+
+            // String interpolation
+            string output = $"{DateTime.Now.ToString(),-24}{note}";
+            // String composition
+            //string output = string.Format(outputFormat, DateTime.Now.ToString(), note);
+            File.AppendAllText(_fileName, output + Environment.NewLine);
+        }
+
+        public string[] GetAllNotes()
+        {
+            string[] input = File.ReadAllLines(_fileName);
+            return input[2..];
+        }
+    }
+}
--- FilesReadWrite/FilesReadWrite/Program.cs
+++ FilesReadWrite/FilesReadWrite/Program.cs
@@ -8,6 +8,26 @@ namespace FilesReadWrite
     {
         static void Main(string[] args)
         {
+            NoteBook noteBook = new NoteBook();
+            string[] notes = noteBook.GetAllNotes();
+            Console.WriteLine("Present notes:");
+            foreach (string note in notes)
+            {
+                Console.WriteLine(note);
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Write note:");
+                string input = Console.ReadLine();
+
+                if (input == "")
+                {
+                    break;
+                }
+
+                noteBook.TakeNote(input);
+            }
         }
     }
 }
